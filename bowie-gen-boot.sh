2a8fddc775e470c853bc6db989a909aed1da2444
Kenny Chen <knych.csy@gmail.com>
2014-07-20 16:47:48 +0000

    Add bowie script.bin generation script

---
+++ bowie-gen-boot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SDBOOTPART=$1
+
+[ -z "$SDBOOTPART" ] && echo Please specify SDCARD boot partition device node && exit 1
+
+# Please do `make' first
+
+mount $SDBOOTPART /opt
+
+./fex2bin ../sunxi-boards/sys_config/a20/cubieboard2.fex /opt/script.bin
+
+echo << EOF > /opt/boot.cmd
+setenv bootargs console=ttyS0,115200 root/dev/mmcblk0p2 init=/sbin/init rootwait panic=10 ${extra}
+fatload mmc 0 0x43000000 script.bin
+fatload mmc 0 0x48000000 uImage
+bootm 0x48000000
+EOF
+
+sync
+
+mkimage -C none -A arm -T script -d /opt/boot.cmd /opt/boot.scr
+
+umount /opt
+
